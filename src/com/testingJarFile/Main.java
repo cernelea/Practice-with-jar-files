f754aa75cede6b86b28f1baa2145b259bf13a3e9
Andrei <cerneleaaa@gmail.com)>
2018-09-07 12:22:15 +0300

    Jar Practice

---
+++ src/com/testingJarFile/Main.java
@@ -0,0 +1,18 @@
+
+package com.testingJarFile;
+
+import com.cernelea.Series;
+
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Series.nsum(5);
+        Series.factorial(5);
+        Series.fibonacci(5);
+        
+        
+    }
+    
+}
